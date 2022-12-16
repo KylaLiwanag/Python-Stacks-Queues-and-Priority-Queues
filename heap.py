9b37914440d7bea06044909c1da7916e35f1435d
KylaLiwanag <kylacamilleliwanag@gmail.com>
2022-12-17 01:51:09 +0800

    FIFO queue

---
+++ heap.py
@@ -0,0 +1,23 @@
+from heapq import heappush
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+fruits
+
+from heapq import heappop
+
+heappop(fruits)
+
+
+fruits
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+person1 < person2
+
+person2 < person3
--- queues.py
+++ queues.py
@@ -11,3 +11,52 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
+
+from queues import Queue
+
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+
+fifo.dequeue()
+
+fifo.dequeue()
+
+fifo.dequeue()
+
+# queues.py
+
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+from queues import Queue
+
+fifo = Queue("1st", "2nd", "3rd")
+len(fifo)
+
+
+for element in fifo:
+    print(element)
+
+
+
+
+
+len(fifo)
