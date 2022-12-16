7a6766f121a8f2a9158e282268b9439659b174a4
KylaLiwanag <kylacamilleliwanag@gmail.com>
2022-12-17 02:15:03 +0800

    Handling Corner Cases in Your Priority Queue

---
+++ corner classes.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class Message:
+    event: str
+
+
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
+
+wipers < hazard_lights
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
+
+
+messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
--- queues.py
+++ queues.py
@@ -31,7 +31,33 @@ class PriorityQueue:
     def dequeue(self):
         return heappop(self._elements)
 
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
 
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
+
+from collections import deque
+from heapq import heappop, heappush
+from itertools import count
+
+# ...
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
 
 
 
